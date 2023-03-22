984c5ea75a098ff461394af37652b048da414761
CoHuK <cohuk2002@gmail.com>
2023-03-22 03:17:57 +0200

    Add Permissions and user management

--- gpt-telegram-bot.py
+++ gpt-telegram-bot.py
@@ -15,34 +15,65 @@ logger = logging.getLogger(__name__)
 load_dotenv()
 TELEGRAM_API_TOKEN = os.getenv('TELEGRAM_API_TOKEN')
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-ALLOWED_USER_IDS = [os.getenv('BOT_ADMIN_USER_ID')]
+ADMIN_ID = os.getenv('BOT_ADMIN_USER_ID')
+ALLOWED_USER_IDS = [ADMIN_ID]
 
 # Initialize the OpenAI library
 openai.api_key = OPENAI_API_KEY
 
+async def allowed_only(update, allowed_list):
+    user_id = update.message.from_user.id
+    if str(user_id) not in allowed_list:
+        await update.message.reply_text("Sorry, you are not authorized to use this bot.")
+        return False
+    return True
+
+async def admin_only(update):
+    return await allowed_only(update, ADMIN_ID)
+
+async def allowed_users_only(update):
+    return await allowed_only(update, ALLOWED_USER_IDS)
+
 # Function to start the bot
 async def start(update: Update, context: CallbackContext):
     context.user_data["chat context"] = []
-    await update.message.reply_text('Welcome to the ChatGPT-3.5 bot! Type your message and I will respond.')
+    await update.message.reply_text('Welcome to the ChatGPT-3.5 bot! Type your message and I will respond.\nUse /clear do clear discussion context')
 
 async def clear(update: Update, context: CallbackContext):
     context.user_data["chat context"] = []
     logger.info("Context should be cleared by now: " + str(context.user_data["chat context"]))
+    await update.message.reply_text('Context cleared')
   
-async def clear(update: Update, context: CallbackContext):
-    context.user_data["chat context"] = []
-    logger.warning("Context should be cleared by now: " + str(context.user_data["chat context"]))  
+async def add_user(update: Update, context: CallbackContext):
+    if await admin_only(update):
+        ALLOWED_USER_IDS.append(context.args[0])
+        logger.info("User " + context.args[0] + " added to allow list")
+        await users(update, context)
+
+async def delete_user(update: Update, context: CallbackContext):
+    if await admin_only(update):
+        user_index = int(context.args[0])
+        if 1 < user_index <= len(ALLOWED_USER_IDS):
+            await update.message.reply_text("User " + ALLOWED_USER_IDS[user_index - 1] + " is deleted")
+            del ALLOWED_USER_IDS[user_index - 1]
+        else:
+            await update.message.reply_text("Wrong user list index number. Use /users to get the right one.")
+        await users(update, context)
+
+async def users(update: Update, context: CallbackContext):
+    if await allowed_users_only(update):
+        response_strings = ""
+        for i, user in enumerate(ALLOWED_USER_IDS):
+            response_strings += str(i+1) + " " + str(user) + "\n"
+        await update.message.reply_text(response_strings)
     
 # Function to handle text messages
 async def handle_text(update: Update, context: CallbackContext):
     user_text = update.message.text
-    user_id = update.message.from_user.id
-    if str(user_id) in ALLOWED_USER_IDS:
+    if await allowed_users_only(update):
         chatgpt_response = get_chatgpt_response(user_text, context.user_data["chat context"])
-        logger.warning("latest context is: " + str(context.user_data["chat context"]))
+        logger.info("latest context is: " + str(context.user_data["chat context"]))
         await update.message.reply_text(chatgpt_response)
-    else:
-        await update.message.reply_text("Sorry, you are not authorized to use this bot.")
 
 # Function to get a response from ChatGPT-3.5
 def get_chatgpt_response(prompt, chat_context):
@@ -61,6 +92,8 @@ def main():
     application.add_handler(CommandHandler('start', start))
     application.add_handler(CommandHandler('clear', clear))
     application.add_handler(CommandHandler('add_user', add_user))
+    application.add_handler(CommandHandler('users', users))
+    application.add_handler(CommandHandler('delete_user', delete_user))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_text))
 
     # Start the bot
